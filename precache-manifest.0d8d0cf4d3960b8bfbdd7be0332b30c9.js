bf613c284577272522cb13d391985359c4db8e47
Aneta <anc@praqma.net>
2020-02-10 03:55:20 +0100

    Updates

---
+++ precache-manifest.0d8d0cf4d3960b8bfbdd7be0332b30c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0549d129640dd407890b74d94c68152f",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "5dc1c7871fdf0583836d",
+    "url": "/movie-search/static/css/2.671adc2c.chunk.css"
+  },
+  {
+    "revision": "46e3fc37e55cb686b211",
+    "url": "/movie-search/static/css/main.728845a0.chunk.css"
+  },
+  {
+    "revision": "5dc1c7871fdf0583836d",
+    "url": "/movie-search/static/js/2.c835707b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie-search/static/js/2.c835707b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46e3fc37e55cb686b211",
+    "url": "/movie-search/static/js/main.197f50fb.chunk.js"
+  },
+  {
+    "revision": "ac6db32057b38b5a9347",
+    "url": "/movie-search/static/js/runtime-main.6bcaf96f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movie-search/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movie-search/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movie-search/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movie-search/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movie-search/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import "font-awesome/css/font-awesome.css";
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
